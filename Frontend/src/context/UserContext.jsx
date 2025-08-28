e5afa348c20bbd81350fadd9909e6d324ac0be96
Reeshi-Raj <reeshiraj707@gmail.com>
2025-08-28 22:39:01 +0530

    made userContext and signup page

--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { createContext, useState } from "react";
 
-const UserContext = () => {
+export const UserContext = createContext({});
+
+export function UserContextProvider({ children }) {
+  const [userInfo, setUserInfo] = useState({});
   return (
-    <div>
-      
-    </div>
-  )
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
-
-export default UserContext
