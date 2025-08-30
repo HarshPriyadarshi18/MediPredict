6404d9f0b8b6451831437bbed7f8e2e4abfcbcbf
Reeshi-Raj <reeshiraj707@gmail.com>
2025-08-30 23:10:15 +0530

    Made NavBar component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <HomePage/>
     </div>
   )
